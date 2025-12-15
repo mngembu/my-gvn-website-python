ead4bf1206ba13b75efb4cd5ff29c5efbe0aa16c
mngembu <mary.ebob@yahoo.ca>
2025-12-15 05:19:36 -0700

    add logo route

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def home():
     return render_template('index.html')
 
+@app.route('/logo')
+def serve_logo():
+    return send_from_directory('static/images', 'gvn-logo.png')
+
 @app.route('/robots.txt')
 def robots():
     return send_from_directory('.', 'robots.txt')
